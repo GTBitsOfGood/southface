25c35f9ed2e739752471460841251ab85d509184
nmhossain02 <nmhossain02@gmail.com>
2022-09-15 12:28:03 -0400

    Added searchString and searchBar in library screen

--- src/screens/Library/LibraryPage.jsx
+++ src/screens/Library/LibraryPage.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 
 import StandardCard from "src/components/StandardCard";
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 const LibraryPage = () => {
   const [books, setBooks] = useState([]);
+  const [searchString, setSearchString] = useState("");
 
   const [example, setExample] = useState("");
   useEffect(() => {
@@ -12,7 +14,13 @@ const LibraryPage = () => {
 
   return (
     <>
+      <SearchBar setSearchString={setSearchString} />
       <p>{example}</p>
+      <p>
+        {searchString === ""
+          ? "No searchString"
+          : "The search string is: " + searchString}
+      </p>
       <StandardCard />
     </>
   );
