55cb0e38449b0d06f18a6b77ca6194227b0bb1d3
cvxluo <cvxluo@gmail.com>
2022-10-29 18:56:19 -0400

    Basic upload functionality

--- src/screens/Library/LibraryPage.jsx
+++ src/screens/Library/LibraryPage.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
-import { Heading } from "@chakra-ui/react";
+import { useState, createRef } from "react";
+import { Heading, Box, Button } from "@chakra-ui/react";
 import SearchBar from "../../components/SearchBar";
 import StandardCardTable from "src/components/StandardCardTable";
 import useUser from "src/utils/lib/useUser";
+
+import { useEffect } from "react";
+import { uploadFile, listBlobs } from "../../utils/blobStorage";
+import FilePicker from "chakra-ui-file-picker";
 
 const LibraryPage = ({ cards }) => {
   const { user: currentUser } = useUser({
@@ -11,12 +15,31 @@ const LibraryPage = ({ cards }) => {
   });
 
   const [searchString, setSearchString] = useState("");
+  const [fileList, setFileList] = useState([]);
+
+  const myRef = createRef();
+
+  const upload = () => {
+    const file = fileList[0];
+    uploadFile(file.name, file, { test_metadata: "test" });
+  };
 
   return (
     <>
       <SearchBar setSearchString={setSearchString} />
       <Heading fontSize={{ base: "4xl", lg: "5xl" }}> Library</Heading>
 
+      <Box>
+        <FilePicker
+          onFileChange={(fileList) => setFileList(fileList)}
+          placeholder="placeholder"
+          clearButtonLabel="Clear Selected Files"
+          multipleFiles={false}
+          hideClearButton={false}
+          ref={myRef}
+        />
+        <Button onClick={upload}>Upload</Button>
+      </Box>
       <StandardCardTable
         cards={cards}
         isLoggedIn={currentUser?.isLoggedIn}
---
+++ src/utils/blobStorage.js
@@ -0,0 +1,41 @@
+import { BlobServiceClient } from "@azure/storage-blob";
+
+const getBlobClient = () => {
+  const blobServiceClient = new BlobServiceClient(
+    `https://${process.env.NEXT_PUBLIC_STORAGERESOURCENAME}.blob.core.windows.net/?${process.env.NEXT_PUBLIC_STORAGESASTOKEN}`
+  );
+  const containerClient = blobServiceClient.getContainerClient("standards");
+
+  return containerClient;
+};
+
+const uploadFile = async (blobName, content, metadata) => {
+  const containerClient = getBlobClient();
+
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+  const options = {
+    blobHTTPHeaders: {
+      blobContentType: content.type,
+    },
+    metadata: metadata,
+  };
+
+  const uploadBlobResponse = await blockBlobClient.uploadBrowserData(
+    content,
+    options
+  );
+  return uploadBlobResponse;
+};
+
+const listBlobs = async () => {
+  const containerClient = getBlobClient();
+
+  let blobs = [];
+  for await (const blob of containerClient.listBlobsFlat()) {
+    blobs.push(blob);
+  }
+
+  return blobs;
+};
+
+export { uploadFile, listBlobs };
