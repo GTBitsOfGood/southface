b3895508acc5e383c33932f47d08280df51c644a
nmhossain02 <nmhossain02@gmail.com>
2023-01-28 17:35:33 -0500

    feat: updated project-plan layout

--- src/pages/plan-builder.jsx
+++ src/pages/plan-builder.jsx
@@ -1,22 +1,25 @@
 import {
-  Box,
   Button,
+  Card,
+  CardBody,
   Flex,
   // Grid,
   Heading,
   HStack,
   Input,
+  StackDivider,
   useDisclosure,
+  VStack,
 } from "@chakra-ui/react";
 import { PDFDownloadLink } from "@react-pdf/renderer";
-import Link from "next/link";
+// import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import { getCards } from "src/actions/Card";
 import { createPlan } from "src/actions/Plan";
 import PlanConfirmationModal from "src/components/Modals/PlanConfirmationModal";
 import PlanDocumentPDF from "src/components/PlanDocumentPDF/PlanDocumentPDF";
 // import SearchBar, { useSearch } from "src/components/SearchBar";
-import StandardCard from "src/components/StandardCard";
+// import StandardCard from "src/components/StandardCard";
 import useUser from "src/lib/hooks/useUser";
 
 const ProjectPlanBuilder = () => {
@@ -48,21 +51,21 @@ const ProjectPlanBuilder = () => {
 
   // Every wrapped card object has an index which can be used to "easily" change
   // state in this component from a <SelectableCard /> child component
-  const SelectionSetter = (index) => {
-    return (selection) => {
-      if (isSwitching) {
-        if (switchWith === null) {
-          setSwitchWith(index);
-        } else {
-          Switcher(index);
-        }
-      } else {
-        const newSelections = [...selections];
-        newSelections[index].selection = selection;
-        setSelections(newSelections);
-      }
-    };
-  };
+  // const SelectionSetter = (index) => {
+  //   return (selection) => {
+  //     if (isSwitching) {
+  //       if (switchWith === null) {
+  //         setSwitchWith(index);
+  //       } else {
+  //         Switcher(index);
+  //       }
+  //     } else {
+  //       const newSelections = [...selections];
+  //       newSelections[index].selection = selection;
+  //       setSelections(newSelections);
+  //     }
+  //   };
+  // };
 
   // Array map methods for rendering
   // const RenderCards = (card) => {
@@ -75,47 +78,47 @@ const ProjectPlanBuilder = () => {
 
   const Comparator = (a, b) => a.order - b.order;
 
-  const RenderSelected = (card) => {
-    const cardProps = { ...card.cardProps };
-    cardProps.selected = card.selection;
-    cardProps.setSelection = SelectionSetter(card.index);
-    cardProps.selectable = true;
-    cardProps.mode = (function () {
-      if (isSwitching) {
-        return card.index === switchWith ? "switchYellow" : "switchGray";
-      } else {
-        return "red";
-      }
-    })();
-    return <StandardCard key={card.index} card={cardProps} />;
-  };
+  // const RenderSelected = (card) => {
+  //   const cardProps = { ...card.cardProps };
+  //   cardProps.selected = card.selection;
+  //   cardProps.setSelection = SelectionSetter(card.index);
+  //   cardProps.selectable = true;
+  //   cardProps.mode = (function () {
+  //     if (isSwitching) {
+  //       return card.index === switchWith ? "switchYellow" : "switchGray";
+  //     } else {
+  //       return "red";
+  //     }
+  //   })();
+  //   return <StandardCard key={card.index} card={cardProps} />;
+  // };
 
   // Order handlers
-  const [isSwitching, setSwitching] = useState(false);
-  useEffect(() => {
-    if (!isSwitching) {
-      setSwitchWith(null);
-    }
-  }, [isSwitching]);
-  const [switchWith, setSwitchWith] = useState(null);
-  const Switcher = (b) => {
-    const a = switchWith;
-    console.log(
-      `Before: a is ${selections[a].order} and b is ${selections[b].order}`
-    );
-    const newSelections = [...selections];
-    const temp = newSelections[a].order;
-    newSelections[a].order = newSelections[b].order;
-    newSelections[b].order = temp;
+  // const [isSwitching, setSwitching] = useState(false);
+  // useEffect(() => {
+  //   if (!isSwitching) {
+  //     setSwitchWith(null);
+  //   }
+  // }, [isSwitching]);
+  // const [switchWith, setSwitchWith] = useState(null);
+  // const Switcher = (b) => {
+  //   const a = switchWith;
+  //   console.log(
+  //     `Before: a is ${selections[a].order} and b is ${selections[b].order}`
+  //   );
+  //   const newSelections = [...selections];
+  //   const temp = newSelections[a].order;
+  //   newSelections[a].order = newSelections[b].order;
+  //   newSelections[b].order = temp;
 
-    setSelections(() => {
-      setSwitching(false);
-      return newSelections;
-    });
-    console.log(
-      `After: a is ${newSelections[a].order} and b is ${newSelections[b].order}`
-    );
-  };
+  //   setSelections(() => {
+  //     setSwitching(false);
+  //     return newSelections;
+  //   });
+  //   console.log(
+  //     `After: a is ${newSelections[a].order} and b is ${newSelections[b].order}`
+  //   );
+  // };
 
   // DB action handlers
   const DiscardPlanHandler = () => {
@@ -159,18 +162,101 @@ const ProjectPlanBuilder = () => {
 
   return (
     <>
-      <Flex flexFlow="row nowrap" mb="10">
-        <Button opacity="0" cursor="default">
-          View Saved Project Plans
-        </Button>
-        <Heading flex="1" gridArea="stack" textAlign="center">
-          Project Plan Builder
-        </Heading>
+      <HStack alignItems="flex-start" spacing={3}>
+        <VStack
+          flex={2}
+          as={Card}
+          alignItems="flex-start"
+          p={6}
+          spacing={5}
+          divider={<StackDivider />}
+        >
+          <CardBody m={-3} w="100%">
+            <Flex mb={3} width="100%" flexFlow="row nowrap">
+              <HStack w="100%" alignItems="flex-start">
+                {hasLoaded ? ( // temp placeholder condition
+                  <Heading maxW="80%" mr={3}>
+                    Title of Current Project Plan
+                  </Heading>
+                ) : (
+                  <Input
+                    size="lg"
+                    maxW="50%"
+                    fontSize="3xl"
+                    variant="flushed"
+                    mr={3}
+                    placeholder="Title of Current Project Plan"
+                    ref={nameRef}
+                  />
+                )}
+                <Button>Rename</Button>
+              </HStack>
+              <Button onClick={onOpen} bg="gold" color="white">
+                End Project Plan
+              </Button>
+            </Flex>
+            <HStack>
+              {hasLoaded && (
+                <PDFDownloadLink
+                  document={
+                    <PlanDocumentPDF
+                      selectedPlanCards={selections
+                        .filter((card) => card.selection)
+                        .map(UnwrapToCard)}
+                    />
+                  }
+                  fileName="plan.pdf"
+                >
+                  {({ loading }) => (
+                    <Button>
+                      {loading
+                        ? "Loading document..."
+                        : "PDF (Jk also download for now lol)"}
+                    </Button>
+                  )}
+                </PDFDownloadLink>
+              )}
+              {hasLoaded && (
+                <PDFDownloadLink
+                  document={
+                    <PlanDocumentPDF
+                      selectedPlanCards={selections
+                        .filter((card) => card.selection)
+                        .map(UnwrapToCard)}
+                    />
+                  }
+                  fileName="plan.pdf"
+                >
+                  {({ loading }) => (
+                    <Button>
+                      {loading ? "Loading document..." : "Download"}
+                    </Button>
+                  )}
+                </PDFDownloadLink>
+              )}
+            </HStack>
+          </CardBody>
+          <CardBody>Bruhas;dlfkjasdfl;ksldlsldllslsldkdksldksld</CardBody>
+        </VStack>
+        <VStack flex={1}>
+          <Card w="100%">
+            <Heading size="lg" p={5}>
+              Saved Project Plans
+            </Heading>
+          </Card>
+          <Card w="100%">
+            <Heading size="lg" p={5}>
+              Recent Standards
+            </Heading>
+          </Card>
+        </VStack>
+      </HStack>
+      {/* <Flex flexFlow="row nowrap" mb="10">
         <Link href="/saved-project-plans">
           <Button href="/">View Saved Project Plans</Button>
         </Link>
-      </Flex>
-      <Flex
+      </Flex> */}
+      {/* <Flex
         p={5}
         borderRadius={15}
         flexDirection="column"
@@ -188,19 +274,6 @@ const ProjectPlanBuilder = () => {
           width="100%"
           alignItems="flex-start"
         >
-          <Box minWidth="40%" as="span">
-            <Input
-              type="text"
-              placeholder="Name your project plan"
-              fontSize="3xl"
-              variant="flushed"
-              mt="3"
-              pb="3"
-              borderBottomWidth="3px"
-              borderColor="darkgray"
-              ref={nameRef}
-            />
-          </Box>
           <HStack>
             <Button
               bgColor={isSwitching ? "gold" : "default"}
@@ -208,25 +281,6 @@ const ProjectPlanBuilder = () => {
               onClick={() => setSwitching(!isSwitching)}
             >
               {isSwitching ? "Finish reordering" : "Reorder cards"}
-            </Button>
-            {hasLoaded && (
-              <PDFDownloadLink
-                document={
-                  <PlanDocumentPDF
-                    selectedPlanCards={selections
-                      .filter((card) => card.selection)
-                      .map(UnwrapToCard)}
-                  />
-                }
-                fileName="plan.pdf"
-              >
-                {({ loading }) => (
-                  <Button>{loading ? "Loading document..." : "PDF"}</Button>
-                )}
-              </PDFDownloadLink>
-            )}
-            <Button onClick={onOpen} bg="gold" color="white">
-              End Project Plan
             </Button>
           </HStack>
         </Flex>
@@ -243,7 +297,7 @@ const ProjectPlanBuilder = () => {
             library to begin adding to your project plan.
           </Box>
         )}
-      </Flex>
+      </Flex> */}
       <PlanConfirmationModal
         isOpen={isOpen}
         onClose={onClose}
