46646b672c8990f5480afe8a1ba78cf271cebd66
tawsif <tawsifkamal123@gmail.com>
2023-02-14 13:33:04 -0500

    style: prettier stuff

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,7 +4,6 @@ import React from "react";
 import NavBar from "src/components/NavBar/NavBar";
 import { SWRConfig } from "swr";
 
-
 import fetchJson from "src/lib/utils/fetchJson";
 import { southfaceTheme } from "src/styles/theme";
 
