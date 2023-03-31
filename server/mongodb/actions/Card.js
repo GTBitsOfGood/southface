a67bdc48ba71be72373bea6b017229eb0f92f4b5
tawsifkamal <tawsifkamal123@gmail.com>
2023-03-31 11:28:07 -0400

    fix: switching to tags and title

--- server/mongodb/actions/Card.js
+++ server/mongodb/actions/Card.js
@@ -56,7 +56,7 @@ export async function getCardsPagination({
       ...query,
       $or: [
         {
-          $or: [
+          $and: [
             { title: { $regex: regex } },
             { "notes.body": { $regex: regex } },
           ],
@@ -107,7 +107,7 @@ export async function getCardsCount({
       ...query,
       $or: [
         {
-          $or: [
+          $and: [
             { title: { $regex: regex } },
             { "notes.body": { $regex: regex } },
           ],
