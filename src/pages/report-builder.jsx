9842f298820478dea6881370e265bcc4b91e14ed
rdong46 <rickydongcollege@gmail.com>
2023-04-08 16:13:40 -0400

    feat: show no report when current report is empty

--- src/pages/report-builder.jsx
+++ src/pages/report-builder.jsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardBody,
@@ -6,20 +7,22 @@ import {
   Heading,
   HStack,
   Input,
+  Link,
   StackDivider,
-  VStack,
+  Text,
   useDisclosure,
+  VStack,
 } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
-import { addToArchivedReport } from "../actions/User/ArchivedReport";
 import ArchivedReportView from "src/components/ArchivedReportView";
 import RecentStandardsView from "src/components/RecentStandardsView";
 import { ReportStandard } from "src/components/StandardCard";
 import useActiveReport from "src/lib/hooks/useActiveReport";
+import { addToArchivedReport } from "../actions/User/ArchivedReport";
+import ConfirmActionModal from "../components/Modals/ConfirmActionModal";
 import PrintToPDFButton from "../components/PrintToPDFButton";
 import useUser from "../lib/hooks/useUser";
-import { useRouter } from "next/router";
-import ConfirmActionModal from "../components/Modals/ConfirmActionModal";
 
 const ReportBuilder = () => {
   // For PDF exporting
@@ -70,80 +73,107 @@ const ReportBuilder = () => {
   return (
     <>
       <HStack py={10} alignItems="flex-start" spacing={3} px={8}>
-        <VStack
-          flex={2}
-          as={Card}
-          alignItems="flex-start"
-          p={6}
-          spacing={5}
-          divider={<StackDivider color="gray.300" />}
-          rounded={16}
-          border="1px solid"
-          borderColor="gray.300"
-        >
-          <CardBody m={-3} w="100%">
-            <Flex
-              mb={3}
-              width="100%"
-              flexFlow="row nowrap"
-              justifyContent="space-between"
-            >
-              <HStack w="100%">
-                {editingTitle ? ( // temp placeholder condition
-                  <Heading maxW="80%" mr={3}>
-                    Untitiled Report
-                  </Heading>
-                ) : (
-                  <Input
-                    size="lg"
-                    maxW="50%"
-                    fontSize="3xl"
-                    variant="flushed"
-                    mr={3}
-                    placeholder="Title of Current Project Plan"
-                    ref={nameRef}
-                  />
-                )}
-                <Button minW="10%" variant="Grey-outlined-rounded">
-                  Rename
-                </Button>
-              </HStack>
-              <Button
-                minW="20%"
-                position="absolute"
-                right="12"
-                variant="Blue-rounded"
-                onClick={onOpen}
-                isDisabled={report?.cards?.length == 0}
+        {!(sels?.length > 0) ? (
+          <VStack
+            as={Card}
+            alignItems="flex-start"
+            p={6}
+            gap={8}
+            rounded={16}
+            border="1px solid"
+            borderColor="gray.300"
+            height="43em"
+          >
+            <Box>
+              {" "}
+              <Text as="b" fontSize="2xl">
+                Current Report
+              </Text>
+              <Text fontSize="xl">
+                {"You haven’t started building a current report yet. " +
+                  "Browse the digital library to begin adding to your current report."}
+              </Text>
+            </Box>
+            <Link href="/library">
+              <Button variant="Grey-rounded">Browse Digital Library</Button>
+            </Link>
+          </VStack>
+        ) : (
+          <VStack
+            flex={2}
+            as={Card}
+            alignItems="flex-start"
+            p={6}
+            spacing={5}
+            divider={<StackDivider color="gray.300" />}
+            rounded={16}
+            border="1px solid"
+            borderColor="gray.300"
+          >
+            <CardBody m={-3} w="100%">
+              <Flex
+                mb={3}
+                width="100%"
+                flexFlow="row nowrap"
+                justifyContent="space-between"
               >
-                Complete Report
-              </Button>
-              <ConfirmActionModal
-                isOpen={isOpen}
-                onClose={onClose}
-                mainText="Would you like to save this report to Completed Reports"
-                confirmButtonText="Yes, complete and save report"
-                cancelButtonText="No, complete without saving report"
-                handleAction={handleCompleteReport}
-                handleCancelAction={() =>
-                  handleCompleteReport({ noSave: true })
-                }
-                isDanger={false}
-                size="2xl"
-              />
-            </Flex>
-            <PrintToPDFButton report={report} />
-          </CardBody>
-          {sels.map((cardWrapper, index) => (
-            <CardBody pl={3} py={0} key={index}>
-              <ReportStandard
-                card={cardWrapper.card}
-                selState={cardWrapper}
-                useGlobalEditing={useGlobalEditing}
-              />
+                <HStack w="100%">
+                  {editingTitle ? ( // temp placeholder condition
+                    <Heading maxW="80%" mr={3}>
+                      Untitiled Report
+                    </Heading>
+                  ) : (
+                    <Input
+                      size="lg"
+                      maxW="50%"
+                      fontSize="3xl"
+                      variant="flushed"
+                      mr={3}
+                      placeholder="Title of Current Project Plan"
+                      ref={nameRef}
+                    />
+                  )}
+                  <Button minW="10%" variant="Grey-outlined-rounded">
+                    Rename
+                  </Button>
+                </HStack>
+                <Button
+                  minW="20%"
+                  position="absolute"
+                  right="12"
+                  variant="Blue-rounded"
+                  onClick={onOpen}
+                  isDisabled={report?.cards?.length == 0}
+                >
+                  Complete Report
+                </Button>
+                <ConfirmActionModal
+                  isOpen={isOpen}
+                  onClose={onClose}
+                  mainText="Would you like to save this report to Completed Reports"
+                  confirmButtonText="Yes, complete and save report"
+                  cancelButtonText="No, complete without saving report"
+                  handleAction={handleCompleteReport}
+                  handleCancelAction={() =>
+                    handleCompleteReport({ noSave: true })
+                  }
+                  isDanger={false}
+                  size="2xl"
+                />
+              </Flex>
+              <PrintToPDFButton report={report} />
             </CardBody>
-          ))}
-        </VStack>
+            {sels.map((cardWrapper, index) => (
+              <CardBody pl={3} py={0} key={index}>
+                <ReportStandard
+                  card={cardWrapper.card}
+                  selState={cardWrapper}
+                  useGlobalEditing={useGlobalEditing}
+                />
+              </CardBody>
+            ))}
+          </VStack>
+        )}
         <VStack maxW="35%" flex={1} alignItems="end">
           {user?.isLoggedIn && user?.archivedReports.length > 0 && (
             <Card
