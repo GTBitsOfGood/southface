492aef0f6544f99dfbb0460f64c3dbb5748ef773
ahong75 <anthonyhong2003@gmail.com>
2023-09-28 19:55:49 -0400

    fix: added indexing by name

--- server/mongodb/models/BuildingType.js
+++ server/mongodb/models/BuildingType.js
@@ -6,8 +6,10 @@ const BuildingTypeSchema = new Schema({
   name: {
     type: String,
     required: true,
+    index: true,
   },
 });
+
 export { BuildingTypeSchema };
 export default mongoose.models?.BuildingType ??
   mongoose.model("BuildingType", BuildingTypeSchema);
