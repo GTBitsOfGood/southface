9d75cf4e30135b46b63c2646e27de51f4910e862
Sophia <sophialin00000@gmail.com>
2023-09-29 23:33:54 -0400

    feat: delete tag

--- src/actions/Tag.js
+++ src/actions/Tag.js
@@ -21,6 +21,27 @@ export const createTag = async (name) => {
     });
 };
 
+export const deleteTag = async (_id) => {
+  return fetch(urls.api.tag.delete, {
+    method: "POST",
+    mode: "same-origin",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ _id }),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      if (json == null) {
+        throw new Error("Could not connect to API!");
+      } else if (!json.success) {
+        throw new Error(json.message);
+      }
+      return json.payload;
+    });
+};
+
 export const getTagsObject = async () => {
   return fetch(urls.api.tag.getObject, {
     method: "GET",
